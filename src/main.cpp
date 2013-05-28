e561785acc423745b1d043a84e491167889c23a1
Weihong Guan <weihong.guan@gmail.com>
2013-05-28 16:41:12 +0800

    temporary remove usart functions and introduce gpio class

---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+/**
+ ******************************************************************************
+ * @file    demo/main.c
+ * @author  aGuegu
+ * @version V3.5.0
+ * @date    May 15, 2013
+ * @brief   Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+ * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+ * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+ * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+ * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+ * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+ *
+ * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include <stdio.h>
+#include "usart/usart.h"
+#include "gpio/gpio.h"
+
+void Delay(u32 count) {
+	extern vu32 TimingDelay;
+	TimingDelay = count;
+	while (TimingDelay)
+		;
+}
+
+Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC );
+Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC );
+
+int main(void) {
+
+	led_green.init(GPIO_Mode_Out_PP);
+	led_blue.init(GPIO_Mode_Out_PP);
+
+	SysTick_Config(SystemCoreClock / 1000);
+
+//	// turn off buffers, so IO occurs immediately
+//	setvbuf(stdin, NULL, _IONBF, 0);
+//	setvbuf(stdout, NULL, _IONBF, 0);
+//	setvbuf(stderr, NULL, _IONBF, 0);
+
+	while (1) {
+		static u8 i = 0;
+
+		//iprintf("%d %s\r\n", i, "Hello, world.");
+//		fprintf(stdout, "%d %s\r\n", i, "stdout");
+//		fprintf(stderr, "%d %s\r\n", i, "stderr");
+
+		led_blue.toggle();
+
+		i++;
+		Delay(1000);
+	}
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t* file, uint32_t line) {
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+	/* Infinite loop */
+	while (1) {
+	}
+}
+#endif
+
+/**
+ * @}
+ */
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
