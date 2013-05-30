bf41853be94527cce9da431098579add434be0d6
Weihong Guan <weihong.guan@gmail.com>
2013-05-30 08:18:44 +0800

    make LibraryHacks.cpp standalone

--- Startup/newlib_stubs.cpp
+++ Startup/newlib_stubs.cpp
@@ -38,46 +38,6 @@ extern int errno;
 char *__env[1] = { 0 };
 char **environ = __env;
 
-/*
- * The default pulls in 70K of garbage
- */
-
-namespace __gnu_cxx {
-void __verbose_terminate_handler() {
-	for (;;)
-		;
-}
-}
-
-/*
- * The default pulls in about 12K of garbage
- */
-
-void __cxa_pure_virtual() {
-	for (;;)
-		;
-}
-
-/*
- * Implement C++ new/delete operators using the heap
- */
-
-void *operator new(size_t size) {
-	return malloc(size);
-}
-
-void *operator new[](size_t size) {
-	return malloc(size);
-}
-
-void operator delete(void *p) {
-	free(p);
-}
-
-void operator delete[](void *p) {
-	free(p);
-}
-
 int _write(int file, const char *ptr, int len);
 
 void _exit(int status) {
@@ -175,28 +135,28 @@ int _lseek(int file, int ptr, int dir) {
  Increase program data space.
  Malloc and related functions depend on this
  */
-caddr_t _sbrk(int incr) {
-
-	extern char _ebss; // Defined by the linker
-	static char *heap_end;
-	char *prev_heap_end;
-
-	if (heap_end == 0) {
-		heap_end = &_ebss;
-	}
-	prev_heap_end = heap_end;
-
-	char * stack = (char*) __get_MSP();
-	if (heap_end + incr > stack) {
-		_write(STDERR_FILENO, "Heap and stack collision\n", 25);
-		errno = ENOMEM;
-		return (caddr_t) -1;
-		//abort ();
-	}
-
-	heap_end += incr;
-	return (caddr_t) prev_heap_end;
-}
+//caddr_t _sbrk(int incr) {
+//
+//	extern char _ebss; // Defined by the linker
+//	static char *heap_end;
+//	char *prev_heap_end;
+//
+//	if (heap_end == 0) {
+//		heap_end = &_ebss;
+//	}
+//	prev_heap_end = heap_end;
+//
+//	char * stack = (char*) __get_MSP();
+//	if (heap_end + incr > stack) {
+//		_write(STDERR_FILENO, "Heap and stack collision\n", 25);
+//		errno = ENOMEM;
+//		return (caddr_t) -1;
+//		//abort ();
+//	}
+//
+//	heap_end += incr;
+//	return (caddr_t) prev_heap_end;
+//}
 
 /*
  read
