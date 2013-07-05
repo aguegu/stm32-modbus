3579d8a7ce1a7cbc59fad7ab659c5d30c857aa62
Weihong Guan <weihong.guan@gmail.com>
2013-07-05 11:13:54 +0800

    sort includes in stm32-template.h

--- inc/stm32-template.h
+++ inc/stm32-template.h
@@ -9,14 +9,17 @@
 #define STM32_TEMPLATE_H_
 
 #include "stm32f10x.h"
-#include <stdio.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
 #include "usart/usart.h"
 #include "gpio/gpio.h"
 #include "tim/tim.h"
 #include "nvic/nvic.h"
-#include <vector>
-#include "stdlib.h"
 #include "i2c/i2c.h"
+#include "adc/adc.h"
 
 void init();
 extern void setup();
