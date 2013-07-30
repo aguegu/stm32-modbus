757b6e1be84f5cf4b7dbd8d6e974453d0580bb72
Weihong Guan <weihong.guan@gmail.com>
2013-07-30 14:43:52 +0800

    format adc result in test script

--- test/md_readbit.py
+++ test/md_readbit.py
@@ -13,15 +13,16 @@ print x.read_bit(0, 1)
 x.write_bit(0, 0, 15)
 print x.read_bit(0, 1)
 
-print x.read_register(0, 0, 4, False)
-print x.read_register(1, 0, 4, False)
+adc = x.read_register(0, 0, 4, False)
+print "%4d\t%.2f%%" % (adc, adc / 40.95)
+adc = x.read_register(1, 0, 4, False)
+print "%4d\t%.2f%%" % (adc, adc / 40.95)
 
 x.write_string(10, "hello", 3)
 print x.read_string(10, 3, 3)
 
 x.write_string(10, "world", 3)
 print x.read_string(10, 3, 3)
-
 
 '''
 print x.write_register(15, 123, 1, 6, False)
