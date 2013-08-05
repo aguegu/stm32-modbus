cc6b373ae4d2eb58b4ce799c18596b5ab90ec02d
Weihong Guan <weihong.guan@gmail.com>
2013-08-05 16:12:21 +0800

    simplify test script and set ttyUSB0 as the RS485 port

--- test/md_readbit.py
+++ test/md_readbit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import minimalmodbus
 
-x = minimalmodbus.Instrument('/dev/ttyUSB1', 2) 
+x = minimalmodbus.Instrument('/dev/ttyUSB0', 2) 
 x.serial.parity = 'E'
 
 print x.read_bit(0, 2)
@@ -25,26 +25,6 @@ x.write_string(10, "world", 3)
 print x.read_string(10, 3, 3)
 
 '''
-print x.write_register(15, 123, 1, 6, False)
-print x.read_register(15, 1, 3, False)
-print x.write_register(0, 0, 0, 6, False)
-print x.read_register(0, 0, 3, False)
-
-print 
-
-print x.write_register(1, 345, 0, 16, False)
-print x.read_register(1, 0, 3, False)
-
-x.write_long(2, 12312)
-print x.read_long(2)
-
-x.write_float(4, 3.14159, 2)
-print x.read_float(4, 3, 2)
-
-x.write_float(6, 3.1415926, 4)
-print x.read_float(6, 3, 4)
-
-
 02050000ff008C09
 020500000000CDf9
 
